637d24f01a17745ac276a00b94ff795ac77467d5
Alexander <alexssjr2002@gmail.com>
2021-11-16 11:35:00 -0500

    TextField array created to manage inputs and nextPage() almost completed

--- src/ui/ProductionsGUI.java
+++ src/ui/ProductionsGUI.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -11,9 +12,14 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import model.CFG;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class ProductionsGUI {
 
     CFG cfg;
+    TextField [][] textFields;
+
     private final double variablesPrefWidth = 22;
     private final double productionsPrefWidth = 247;
     private final double variablesPrefHeight = 25;
@@ -45,14 +51,74 @@ public class ProductionsGUI {
 
     @FXML
     void nextPage(ActionEvent event) {
+        if (hasEmptyFields()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Empty Fields");
+            alert.setContentText("Looks like some of the fields are empty. Check them carefully to proceed.");
 
+            alert.showAndWait();
+        }else if (isWrongFormat()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Wrong Format");
+            alert.setContentText("Some variables or productions are in wrong format. Please fix it.");
+        }else {
+            assignProductions();
+            System.out.println("Productions assigned");
+            if (cfg.isCNF()){
+                System.out.println("Is CNF");
+            }else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Wrong Grammar");
+                alert.setContentText("Your grammar is not in Chomsky Normal Form. Please fix it.");
+                alert.showAndWait();
+            }
+        }
     }
+
+    private void assignProductions() {
+        HashMap<Character, String> productions = new HashMap<>();
+        for (int i = 0; i < textFields.length; i++) {
+            char variable = textFields[i][0].getText().charAt(0);
+            String stringProduction = textFields[i][1].getText();
+            productions.put(variable, stringProduction);
+        }
+        cfg.setProductions(productions);
+    }
+
+    private boolean isWrongFormat() {
+        String variables = Arrays.toString(cfg.getVariables());
+        String symbols = Arrays.toString(cfg.getSymbols());
+        boolean isWrongFormat = false;
+        for (int i = 0; i < textFields.length && !isWrongFormat; i++) {
+            String variableString = textFields[i][0].getText();
+            String productionString = textFields[i][1].getText();
+
+            isWrongFormat = !variableString.matches("[" + variables + "]+") || !productionString.matches("[" + variables + symbols + "|" + "]+");
+        }
+        return isWrongFormat;
+    }
+
+    private boolean hasEmptyFields() {
+        boolean hasEmptyField = false;
+        for (int i = 0; i < textFields.length && !hasEmptyField; i++) {
+            String variableString = textFields[i][0].getText();
+            String productionString = textFields[i][1].getText();
+
+            hasEmptyField = variableString.isEmpty() || productionString.isEmpty();
+        }
+        return hasEmptyField;
+    }
+
 
     public void initialize() {
         fillTable();
     }
 
     private void fillTable() {
+        textFields = new TextField[cfg.getVariables().length][2];
         char [] variablesArray = cfg.getVariables();
         for (int i = 0; i < variablesArray.length; i++) {
             createProduction(variablesArray[i], i);
@@ -72,7 +138,11 @@ public class ProductionsGUI {
         newProduction.setAlignment(alignment);
 
         gridPane.addRow(i + 1, newVariable, newLabel, newProduction);
+        textFields[i][0] = newVariable;
+        textFields[i][1] = newProduction;
         gridPane.setHalignment(newLabel, HPos.CENTER);
 
     }
+
+
 }
